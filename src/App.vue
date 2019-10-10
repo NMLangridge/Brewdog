75f60ab23bc049e283ddee6fddd84cafa7251a25
Nathan Langridge <nathanlangridge@hotmail.com>
2019-10-10 11:13:11 +0100

    Beer List & Detail

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Beers</h1>
+    <beers-list :beers='beers'></beers-list>
+    <beer-detail :beer='selectedBeer'></beer-detail>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { eventBus } from './main.js';
+import BeersList from './components/BeersList.vue';
+import BeerDetail from './components/BeerDetail.vue';
 
 export default {
   name: 'app',
+  data(){
+    return {
+      beers: [],
+      selectedBeer: null
+    };
+  },
+  mounted(){
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(result => result.json())
+    .then(beers => this.beers = beers)
+
+    eventBus.$on('beer-selected', (beer) => {
+      this.selectedBeer = beer;
+    })
+  },
   components: {
-    HelloWorld
+    "beers-list": BeersList,
+    "beer-detail": BeerDetail
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/BeerDetail.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div v-if='beer'>
+    <h2>{{beer.name}}</h2>
+    <p>Name: {{beer.name}}</p>
+    <p>Tagline: {{beer.tagline}}</p>
+    <p>First Brewed: {{beer.first_brewed}}</p>
+    <p>Description: {{beer.description}}</p>
+    <p>Alcohol Level: {{beer.abv}}</p>
+    <p>Food Pairing: {{beer.food_pairing}}</p>
+    <p>Image: {{beer.image_url}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'beer-detail',
+  props: ['beer']
+}
+</script>
+
+<style lang="css" scoped>
+</style>
